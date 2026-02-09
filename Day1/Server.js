79369b36784f30e8ea0b893b6879fa64bd260ef9
git remote add origin https://github.com/Ajaychoudhary22/javascript-revsion.git git branch -M main git push -u origin main <ajayc3498@gmail.com>
2026-02-09 10:29:15 +0530

    how too create server with express package

--- Day1/Server.js
+++ Day1/Server.js
@@ -1,12 +1,13 @@
+
+
 const http = require('http');
-
-//here we are creating a server using http module
-const server = http.createServer((req, res) => {
-    res.end('Hellooooo, World!');  // through this line we can send the request to the server                                          
-});
-// here we are making the server to listen on port 8000
-server.listen(8000, () => {
-    console.log('Server is listening on port 8000');
-});
-
+ 
+ const server = http.createServer((req,res)=>{
+    res.end("hello word to server")  //server pr request bheji jati hai 
+ }) // yha apna server create hoo jata h
+ 
+ server.listen(3000,()=>{
+    console.log("server running on port 3000");
+    
+ })
 
--- Day2/Express.js
+++ Day2/Express.js
@@ -1,14 +1,18 @@
-const Express = require('express');
 
-const app = Express();
+const express = require("express")
 
+const app = express()  // jab ham express ko call karte hai to hame ek object milta hai jise hum app me store kar lete hai. is object ke through hum apne server se related sare kaam kar sakte hai.
 
-// here we are creating a server using express module
+app.get("/",(req,res)=>{
+    res.send("server is started")  // ye ek route hai jisme "/" ka matlab hai root route. jab bhi koi user is route par request bhejta hai to ye function chalega jisme req (request) aur res (response) parameters hote hai. res.send() method se hum response bhejte hai.
+
+})
+
 app.get("/home",(req,res)=>{
-    res.send("Welcome to Home Page");
-});
+    res.send("welcome to the Home Page");
+})
 
-// here we are making the server to listen on port 8000
-app.listen(8000,()=>{
-    console.log("Server is listening on port 8000");
-});
+app.listen(3000,()=>{
+    console.log("running on port number 3000");  // app.listen() method se hum apne server ko kisi specific port par listen karte hai. jab bhi koi user is port par request bhejta hai to ye server us request ko handle karta hai. is method ke andar hum ek callback function bhi de sakte hai jo tab chalega jab server successfully start ho jata hai.
+    
+})
